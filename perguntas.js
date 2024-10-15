8de15cbe3d7e5328a43fa6af128ce0eebe1cf0e6
nicolly <159157320+nickgomesx@users.noreply.github.com>
2024-10-15 12:25:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+'Programação',
+'O que éjavaScript',
+'O javaScript é uma linguagem de programação'
+
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+
+)
+
+criaCartao(
+    'Programaçaõ',
+    'O que é uma função',
+    'Uma função é um bloco de códigos que executa uma função',
+
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o resultado de 6 x 8?',
+    'O resultado é 48',
+)
