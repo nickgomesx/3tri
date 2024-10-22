5a3ecde6684da310885a386c321690471819fe7c
nicolly <159157320+nickgomesx@users.noreply.github.com>
2024-10-22 11:27:29 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,17 +2,24 @@
   let container = document.getElementById('container');
   let cartao = document.createElement('article');
   cartao.className ='cartao';
-  cartao.innerHTML 
-`
- <div class="cartao__conteudo">
-        <h3>${categoria}</h3>
-          <div class="cartao__conteudo__pergunta">
-  <P> ${pergunta}</P>
-     </div>
-      <div class="cartao__conteudo__resposta">
-      <p>${resposta}</p>
-     </div>
-  `
+  cartao.innerHTML =`
+                    <div class="cartao__conteudo">
+                       <h3>${categoria}</h3>
+                       <div class="cartao__conteudo__pergunta">
+                       <P> ${pergunta}</P>
+                      </div>
+                      <div class="cartao__conteudo__resposta">
+                      <p>${resposta}</p>
+                      </div> `
+
+  let respostaEstaVisivel = false
+  function viraCartao () {
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+  }
+  cartao.addEventListener('click', viraCartao)
+
   container.appendChild(cartao)
+  
 }
 
